25127a0c25aa25d70ed10c4c8a6bfd5514fc953a
tet <t@e.t>
2016-12-23 19:36:52 +0100

    optimize

--- Calculating_log/Source.cpp
+++ Calculating_log/Source.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, wchar_t* argv[]) {
 
-	double a = 942992326, b = 11.562346;
+	double a = 92992326, b = 11.562346;
 
 	double r;  //9.553112
 
@@ -12,13 +12,15 @@ int main(int argc, wchar_t* argv[]) {
 	double aa = a;
 	double ri = 0.0;
 
-	while (aa > b){aa /= b; ri++;}
+	double ib = 1.0 / b; // multiplying by an inverse is much faster than division
+
+	while (aa > b){aa *= ib; ri++;}
 
 	double p = 1.0;
 	for (int i = 0; i < 60; i++) {
 		aa *= aa;
 		p /= 2.0;
-		if (aa > b) { ri += p; aa /= b; }
+		if (aa > b) { ri += p; aa *= ib; }
 	}
 
 	printf("\n%lf\n", ri);
